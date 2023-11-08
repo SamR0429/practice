162b11cbd6812ebfe8232824237f2e2e2e255569
SamR0429 <Samanatha.ramos01@gmail.com>
2023-11-08 13:54:42 -0600

    working on scrimba, did some warm ups

---
+++ js/scrimba11-8.js
@@ -0,0 +1,22 @@
+
+//document.querySelector(".count-el").innerHTML = 5 ;
+
+// let count = 0 ;
+// console.log(count);
+
+// let myAge = 24;
+// let humanDogRatio = 7;
+//
+// let result = myAge * humanDogRatio;
+// console.log(result)
+
+let bonusPoints = 50;
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
