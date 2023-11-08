7dad4d4ed181c56684cb46934c64636e73471c2b
SamR0429 <Samanatha.ramos01@gmail.com>
2023-11-08 15:30:32 -0600

    created save button , created some functions

--- js/scrimba11-8.js
+++ js/scrimba11-8.js
@@ -1,6 +1,17 @@
 
 //document.querySelector(".count-el").innerHTML = 5 ;
 
+// let bonusPoints = 50;
+//
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+//
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+//
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
 // let count = 0 ;
 // console.log(count);
 
@@ -10,13 +21,27 @@
 // let result = myAge * humanDogRatio;
 // console.log(result)
 
-let bonusPoints = 50;
 
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints);
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-bonusPoints = bonusPoints - 75;
-console.log(bonusPoints);
+//////////////////////////////////////////////////////////
+let count = 0;
 
-bonusPoints = bonusPoints + 45;
-console.log(bonusPoints);
+
+//////////////////////////////////////////////////////////
+let countEL = document.querySelector(".count-el");
+
+let saveBtn = document.querySelector(".save-btn");
+
+//////////////////////////////////////////////////////////
+function increment(){
+    count = count + 1;
+    countEL.innerHTML = count;
+}
+
+function save(){
+    console.log(count);
+}
