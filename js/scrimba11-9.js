b0636d5131c0d14928331430557d0947b81d424f
SamR0429 <Samanatha.ramos01@gmail.com>
2023-11-09 20:30:18 -0600

    completed question 2

--- js/scrimba11-9.js
+++ js/scrimba11-9.js
@@ -9,3 +9,18 @@
 //
 // greetings();
 
+// practice question 2
+// let myPoints = 3;
+// function add3Points(){
+//  myPoints += 3;
+// }
+// function remove1Point(){
+//     myPoints -= 1;
+// }
+// add3Points();
+// add3Points();
+// add3Points();
+// remove1Point();
+// remove1Point();
+// console.log(myPoints);
+
