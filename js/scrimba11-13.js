b625e1e7beaf840e4169a90983c0e9a07cab345b
SamR0429 <Samanatha.ramos01@gmail.com>
2023-11-14 20:03:24 -0600

    score board functionality done

--- js/scrimba11-13.js
+++ js/scrimba11-13.js
@@ -1,15 +1,38 @@
 
 let homeScore = 0;
+let guestScore = 0;
 
-const scoreSpan = document.querySelector(".score");
+const homeScoreP = document.querySelector(".home-score");
+const guestScoreP = document.querySelector(".guest-score");
 
-// const onePoint = document.querySelector(".add-one");
-// const twoPoint = document.querySelector(".add-two");
-// const threePoint = document.querySelector(".add-three");
 
 function plusOne(){
     homeScore+= 1;
     console.log("clicked add one")
-    scoreSpan.innerHTML = homeScore;
+    homeScoreP.innerHTML = homeScore;
 }
 
+function plusTwo(){
+    homeScore+= 2;
+    homeScoreP.innerHTML = homeScore;
+}
+
+function plusThree(){
+    homeScore+= 3;
+    homeScoreP.innerHTML = homeScore;
+}
+
+function plusOneGuest(){
+    guestScore+=1;
+    guestScoreP.innerHTML = guestScore;
+}
+
+function plusTwoGuest(){
+    guestScore+=2;
+    guestScoreP.innerHTML = guestScore;
+}
+
+function plusThreeGuest(){
+    guestScore+=3;
+    guestScoreP.innerHTML = guestScore;
+}
