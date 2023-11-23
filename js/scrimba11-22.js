ca5635b37cebe395f32351bcc06e6be81ff615b2
SamR0429 <Samanatha.ramos01@gmail.com>
2023-11-22 18:21:41 -0600

    worked on scrimba

---
+++ js/scrimba11-22.js
@@ -0,0 +1,25 @@
+// let person = {
+//     name: "sam",
+//     age: 24,
+//     country: "USA"
+// }
+//
+// function logData(){
+//     return person.name + " is " + person.age + " years old and lives in " + person.country
+// }
+//
+// console.log(logData())
+
+let age = 15;
+
+if (age < 6){
+    console.log("free")
+}else if(age < 18){
+    console.log("child discount")
+}else if (age < 27){
+    console.log("student price")
+}else if (age < 67){
+    console.log("full price")
+}else {
+    console.log("senior citizen")
+}
