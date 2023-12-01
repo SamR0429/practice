f84ad7bf40307e06bb8901bd3aae85d8aa465248
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-01 10:18:31 -0600

    added files for mini excersies 12-01

--- js/scrimba11-30.js
+++ js/scrimba11-30.js
@@ -1,1 +1,9 @@
 
+const inputFeild = document.querySelector(".input-el");
+const inputBtn = document.querySelector(".input-btn");
+
+
+
+inputBtn.addEventListener("click", function (){
+    console.log("button clicked")
+})
