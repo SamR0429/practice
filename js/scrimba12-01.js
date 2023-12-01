7b25b9a6abf274a406abbb494748b686af561d94
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-01 14:17:11 -0600

    created dynamically render button and p tag

--- js/scrimba12-01.js
+++ js/scrimba12-01.js
@@ -3,4 +3,21 @@ const theBox = document.querySelector(".box");
 
 theBox.addEventListener("click", function (){
     console.log("i want to open the box!");
-})
+});
+
+////////////////////////////////////////////////////////////////////////
+
+const container = document.querySelector(".container");
+
+container.innerHTML = "<button class='button'>Buy</button>";
+
+//this part is targeting the button, and on click it will render the paragraph tag. the commented in the version that gave on scrimba which also works too but is using the onclick='' method with a separate function
+const button = document.querySelector(".button");
+button.addEventListener("click", function (){
+    container.innerHTML += "<p>Thank you for buying!</p>"
+});
+
+// container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+// function buy(){
+//     container.innerHTML += "<p>Thank you for buying!</p>"
+// }
