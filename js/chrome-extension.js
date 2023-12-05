d4ffd3472ccaa10d94cda7f631a29843ca274c37
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-04 21:29:26 -0600

    created renderleads function, added href and target to li, used template literals

--- js/chrome-extension.js
+++ js/chrome-extension.js
@@ -1,25 +1,32 @@
-
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 
 const inputEl = document.querySelector(".input-el");
 const inputBtn = document.querySelector(".input-btn");
 const ulEl = document.querySelector(".ul-el");
 
 inputBtn.addEventListener("click", function () {
-    console.log("button clicked")
 
     myLeads.push(inputEl.value);
-
-    console.log(myLeads)
+    inputEl.value = '';
+    renderLeads();
 });
 
-let listItems = '';
+function renderLeads() {
 
-for(let i = 0; i < myLeads.length; i ++){
+    let listItems = '';
 
-    //this is the original version and we refactored to make code work faster ?? and to clean up the code. this is done to wait until the for loop is done iterating to then render so to not mess with the dom every iteration
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-    listItems += "<li>" + myLeads[i] + "</li>";
-}
+    for (let i = 0; i < myLeads.length; i++) {
+
+        //this is the original version and we refactored to make code work faster ?? and to clean up the code. this is done to wait until the for loop is done iterating to then render so to not mess with the dom every iteration
+        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
 
     ulEl.innerHTML = listItems;
+}
