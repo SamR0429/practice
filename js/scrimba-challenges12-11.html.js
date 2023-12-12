535653d11de7a446ca789e76ac2316ad2a6bb95b
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-11 21:09:07 -0600

    review with . nation and event listeners

--- js/scrimba-challenges12-11.html.js
+++ js/scrimba-challenges12-11.html.js
@@ -23,3 +23,27 @@ logItems(myCourses);
 // localStorage.setItem("myCredits", "100");
 let myCredits = localStorage.getItem("myCredits")
 console.log(myCredits);
+
+
+
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    },
+    {
+        player: "Mark",
+        score: 41
+    }
+];
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function (){
+
+    console.log(data[0].score);
+});
+
+// Fetch the button from the DOM, store it in a variable
+// Use addEventListener() to listen for button clicks
+// Log Jane's score when the button is clicked (via data)
