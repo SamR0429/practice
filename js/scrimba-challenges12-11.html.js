d30ee28b9abe21903316a056a67a41437d6c1463
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-12 21:04:26 -0600

    worked with scrimba with another side challenge

--- js/scrimba-challenges12-11.html.js
+++ js/scrimba-challenges12-11.html.js
@@ -47,3 +47,33 @@ btn.addEventListener("click", function (){
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+
+
+
+// The generateSentence(desc, arr) takes two parameterer: a description and an array.
+// It should return a string based upon the description and array.
+
+// Example 1: if you pass in "largest countries",and ["China", "India", "USA"],
+// it should return the string: "The 3 largest countries are China, India, USA"
+
+// Example 2:If you pass in "best fruits" and ["Apples", "Bananas"], it should return:
+// "The 2 best fruits are Apples, Bananas"
+
+// Use both a for loop and a template string to solve the challenge
+function generateSentence(desc, arr) {
+    function generateSentence(desc, arr) {
+        let baseString = `The ${arr.length} ${desc} are `
+        for (let i = 0; i < arr.length; i++) {
+            const lastIndex = arr.length - 1
+            if (i === lastIndex) {
+                baseString += arr[i]
+            } else {
+                baseString += arr[i] + ", "
+            }
+        }
+        return baseString
+    }
+
+}
+const sentence = generateSentence("highest mountains", ["Mount Everest", "K2"])
+console.log(sentence)
