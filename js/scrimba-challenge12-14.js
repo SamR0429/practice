e8be64962742e52b0bca6de920e793561d637c09
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-14 18:45:34 -0600

    added files for unit converter

--- js/scrimba-challenge12-14.js
+++ js/scrimba-challenge12-14.js
@@ -5,6 +5,10 @@
 
 const totalPrice = 420.69235632455
 const btn = document.getElementById("purchase-btn");
-btn.textContent = `Buy €${ Math.round(totalPrice * 10 ** 2) / 10 ** 2 }`
+btn.textContent = `Buy €${ totalPrice.toFixed(2)}`
 
+//if we got the wrong data type for the totalPrice use the code below
 
+// const totalPrice = "420.69235632455";
+// const btn = document.getElementById("purchase-btn");
+// btn.textContent = `Buy €${ Number(totalPrice).toFixed(2)}`
