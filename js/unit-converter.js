95dd97c2a2166ea36e57a242e7643f9d09ac864b
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-18 19:43:30 -0600

    added event listener for functions

--- js/unit-converter.js
+++ js/unit-converter.js
@@ -7,15 +7,23 @@
 
 // will need to change innerHTML of
 
-let input = 0;
+let input = 1;
+let lengthConversion = 0;
+let volumeConversion = 0;
 
 const convertBtn = document.querySelector(".convert-btn");
 
-function lengthConversion(){
-   input = input * 3.281;
-
+function meterFeetConversion(){
+   lengthConversion = input * 3.281;
+    console.log(lengthConversion)
 }
 
-function volumeConversion(){
-
+function literGallonConversion(){
+   volumeConversion = input * 0.264;
+   console.log(volumeConversion)
 }
+
+convertBtn.addEventListener("click", function (){
+    meterFeetConversion()
+    literGallonConversion()
+});
