084df31643bf07d8acf34916031b97162e653787
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-19 20:17:55 -0600

    finished all three functions and change naming convention for clarity

--- js/unit-converter.js
+++ js/unit-converter.js
@@ -1,16 +1,11 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
-// will need to change innerHTML of
 
 let input = 1;
 let metersConversion;
 let feetConversion;
 let litersConversion;
 let gallonConversion;
+let kilogramConversion;
+let poundsConversion;
 
 const convertBtn = document.querySelector(".convert-btn");
 const lengthTxtField = document.querySelector(".length-field-text");
@@ -20,19 +15,26 @@ const massTxtField = document.querySelector(".mass-field-text");
 
 function meterFeetConversion() {
     // console.log(lengthConversion)
-    metersConversion = input * 3.281;
-    feetConversion = input * 0.3048;
-    lengthTxtField.innerHTML = `${input} meters = ${metersConversion} feet | ${input} feet = ${feetConversion} meters`;
+    feetConversion = input * 3.281;
+    metersConversion = input * 0.3048;
+    lengthTxtField.innerHTML = `${input} meters = ${feetConversion} feet | ${input} feet = ${metersConversion} meters`;
 }
 
 function literGallonConversion() {
     // console.log(volumeConversion)
-    litersConversion = input * 0.264;
-    gallonConversion = input * 3.785
-    volumeTxtField.innerHTML = `${input} liters = ${litersConversion} gallons | ${input} gallons = ${gallonConversion} liters`;
+    gallonConversion = input * 0.264;
+    litersConversion = input * 3.785
+    volumeTxtField.innerHTML = `${input} liters = ${gallonConversion} gallons | ${input} gallons = ${litersConversion} liters`;
+}
+
+function kilogramPoundConversion(){
+    poundsConversion = input * 2.204;
+    kilogramConversion = input * 0.453;
+    massTxtField.innerHTML = `${input} kilos = ${poundsConversion} pounds | ${input} pounds = ${kilogramConversion} kilos`
 }
 
 convertBtn.addEventListener("click", function () {
     meterFeetConversion()
     literGallonConversion()
+    kilogramPoundConversion()
 });
