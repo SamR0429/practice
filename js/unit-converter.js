96cbe95be3633e38af62a8dcb6d0304650ca9ede
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-20 19:14:55 -0600

    tweeked functions to take input values and stay to cut off at the hundreths place

--- js/unit-converter.js
+++ js/unit-converter.js
@@ -25,19 +25,19 @@ convertBtn.addEventListener("click", function () {
 });
 
 function meterFeetConversion(inputValue) {
-    feetConversion = inputValue * 3.281;
-    metersConversion = inputValue * 0.3048;
+    feetConversion = (inputValue * 3.281).toFixed(3);
+    metersConversion = (inputValue * 0.304).toFixed(3);
     lengthTxtField.innerHTML = `${inputValue} meters = ${feetConversion} feet | ${inputValue} feet = ${metersConversion} meters`;
 }
 
 function literGallonConversion(inputValue) {
-    gallonConversion = inputValue * 0.264;
-    litersConversion = inputValue * 3.785;
+    gallonConversion = (inputValue * 0.264).toFixed(3);
+    litersConversion = (inputValue * 3.785).toFixed(3);
     volumeTxtField.innerHTML = `${inputValue} liters = ${gallonConversion} gallons | ${inputValue} gallons = ${litersConversion} liters`;
 }
 
 function kilogramPoundConversion(inputValue) {
-    poundsConversion = inputValue * 2.204;
-    kilogramConversion = inputValue * 0.453;
+    poundsConversion = (inputValue * 2.204).toFixed(3);
+    kilogramConversion = (inputValue * 0.453).toFixed(3);
     massTxtField.innerHTML = `${inputValue} kilos = ${poundsConversion} pounds | ${inputValue} pounds = ${kilogramConversion} kilos`;
 }
