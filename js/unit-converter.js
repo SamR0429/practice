45c94c494752f271ecb5fb32b205b13569a75651
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-20 18:30:42 -0600

    connected input tag to js for user input

--- js/unit-converter.js
+++ js/unit-converter.js
@@ -1,5 +1,4 @@
 
-let input = 1;
 let metersConversion;
 let feetConversion;
 let litersConversion;
@@ -7,34 +6,38 @@ let gallonConversion;
 let kilogramConversion;
 let poundsConversion;
 
+const input = document.querySelector(".number-input");
 const convertBtn = document.querySelector(".convert-btn");
 const lengthTxtField = document.querySelector(".length-field-text");
 const volumeTxtField = document.querySelector(".volume-field-text");
 const massTxtField = document.querySelector(".mass-field-text");
 
-
-function meterFeetConversion() {
-    // console.log(lengthConversion)
-    feetConversion = input * 3.281;
-    metersConversion = input * 0.3048;
-    lengthTxtField.innerHTML = `${input} meters = ${feetConversion} feet | ${input} feet = ${metersConversion} meters`;
-}
-
-function literGallonConversion() {
-    // console.log(volumeConversion)
-    gallonConversion = input * 0.264;
-    litersConversion = input * 3.785
-    volumeTxtField.innerHTML = `${input} liters = ${gallonConversion} gallons | ${input} gallons = ${litersConversion} liters`;
-}
-
-function kilogramPoundConversion(){
-    poundsConversion = input * 2.204;
-    kilogramConversion = input * 0.453;
-    massTxtField.innerHTML = `${input} kilos = ${poundsConversion} pounds | ${input} pounds = ${kilogramConversion} kilos`
-}
-
 convertBtn.addEventListener("click", function () {
-    meterFeetConversion()
-    literGallonConversion()
-    kilogramPoundConversion()
+    const inputValue = parseFloat(input.value);
+
+    if (!isNaN(inputValue)) {
+        meterFeetConversion(inputValue);
+        literGallonConversion(inputValue);
+        kilogramPoundConversion(inputValue);
+    } else {
+        alert("Please enter a valid number.");
+    }
 });
+
+function meterFeetConversion(inputValue) {
+    feetConversion = inputValue * 3.281;
+    metersConversion = inputValue * 0.3048;
+    lengthTxtField.innerHTML = `${inputValue} meters = ${feetConversion} feet | ${inputValue} feet = ${metersConversion} meters`;
+}
+
+function literGallonConversion(inputValue) {
+    gallonConversion = inputValue * 0.264;
+    litersConversion = inputValue * 3.785;
+    volumeTxtField.innerHTML = `${inputValue} liters = ${gallonConversion} gallons | ${inputValue} gallons = ${litersConversion} liters`;
+}
+
+function kilogramPoundConversion(inputValue) {
+    poundsConversion = inputValue * 2.204;
+    kilogramConversion = inputValue * 0.453;
+    massTxtField.innerHTML = `${inputValue} kilos = ${poundsConversion} pounds | ${inputValue} pounds = ${kilogramConversion} kilos`;
+}
