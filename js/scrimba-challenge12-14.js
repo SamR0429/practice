2f4254c3bd4b7302c897cda0cbc3fffe25f0bace
SamR0429 <Samanatha.ramos01@gmail.com>
2024-01-02 18:44:53 -0600

    commiting

--- js/scrimba-challenge12-14.js
+++ js/scrimba-challenge12-14.js
@@ -12,3 +12,10 @@ btn.textContent = `Buy €${ totalPrice.toFixed(2)}`
 // const totalPrice = "420.69235632455";
 // const btn = document.getElementById("purchase-btn");
 // btn.textContent = `Buy €${ Number(totalPrice).toFixed(2)}`
+
+//you can delete this later , just trying to get a commit for the day!!
+
+//01/02/2023
+//hello
+//hi
+//see you later
